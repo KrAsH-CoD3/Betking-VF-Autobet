05c4e23b29760816eabdd8f94f5478e84375c6fb
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-03-10 06:48:17 +0100

    Updated: Completed scraping predicted matche results.

--- pred_stat.py
+++ pred_stat.py
@@ -13,7 +13,7 @@ async def run(playwright: Playwright):
     user_data_path: str = os.path.join(current_working_dir, profile)
 
     context = await playwright['chromium'].launch_persistent_context(
-        args = ['--touch-events=enabled', '--disable-dev-shm-usage', '--disable-blink-features=AutomationControlled', '--incognito'],
+        args = ['--touch-events=enabled', '--disable-dev-shm-usage', '--disable-blink-features=AutomationControlled'],
         user_data_dir = user_data_path, 
         headless = False,
         color_scheme ='dark',
@@ -22,19 +22,16 @@ async def run(playwright: Playwright):
         viewport = {'width': 400, 'height': 590},
         device_scale_factor = 2.625,
     )
-
+    
     realnaps_tab = context.pages[0]
     default_timeout: int = 30 * 1000
     realnaps_tab.set_default_navigation_timeout(default_timeout)
     realnaps_tab.set_default_timeout(default_timeout)
     
+    betking_virtual_result: str = 'https://m.betking.com/virtual/league/kings-league/results'
     realnaps_betking: str = "https://realnaps.com/signal/premium/ultra/betking-kings-league.php"
 
-    # Opens Realnaps
-    await realnaps_tab.goto(realnaps_betking, wait_until="commit")
-    with contextlib.suppress():    
-        await realnaps_tab.locator('//p[contains(text(), "Consent")]').click()
-    
+        
     async def get_team() -> list:
         hometeam: str = await realnaps_tab.inner_text('//div[@id="homeTxt" and @class="col"]')
         awayteam: str = await realnaps_tab.inner_text('//div[@id="awayTxt" and @class="col"]')
@@ -50,18 +47,47 @@ async def run(playwright: Playwright):
             return int(await realnaps_tab.inner_text('//span[@id="day"]'))
         return int(weekday)
     
+    betking_tab = await context.new_page()
+    await betking_tab.goto(betking_virtual_result, wait_until="commit")
+
+    # Opens Realnaps
+    await realnaps_tab.goto(realnaps_betking, wait_until="commit")
+    with contextlib.suppress(): await realnaps_tab.locator('//p[contains(text(), "Consent")]').click()
+
+    season_teams: dict = {}
     teams: dict = {}
+    iterate: int = 0
     while True:
         weekday: int = await pred_day()
         for slideno in range(3):
             if slideno != 0: 
                 await realnaps_tab.locator(f'//a[contains(text(), "{slideno+1}")]').click()
-                if slideno == 2:
-                    await realnaps_tab.locator(f'//a[contains(text(), "{1}")]').click()
             teams[f"team{slideno+1}"] = await get_team()
-        print(f"Weekday {str(weekday)}")
-        for key, value in teams.items():
-            print(f"{key}: {value[0]} vs {value[1]}")
+            if slideno == 2: # Click back to the first team 
+                await realnaps_tab.locator(f'//a[contains(text(), "{1}")]').click()
+        
+        # Wait until next prediction is displayed so as get result
+        while True: 
+            new_weekday: int = await pred_day()
+            if weekday == new_weekday: continue
+            break
+        # Reload to get match result
+        await betking_tab.reload()
+        print(f"{'-'*9}Weekday {weekday}{'-'*9}")
+        match_table: str = f'//mvs-tournament-results//div[text()="Week {weekday}"]/../..'
+        for key, value in teams.items(): 
+            score_cell_xpath = f'//div[contains(text(), "{value[0]}")]/..//following-sibling::div[@class="score ft"]'
+            match_score = await betking_tab.locator(match_table+score_cell_xpath).inner_text()
+            home_score: int = int(match_score.split(" - ")[0])
+            away_score: int = int(match_score.split(" - ")[1])
+            teams[key] += [match_score, "WON!" if (home_score + away_score) > 2 else "LOST!"]
+            print(f"{key}: {value[0]} vs {value[1]}: {match_score} {value[-1]}")
+        iterate += 1
+        # if iterate > 5:
+        #     await asyncio.sleep(120 * 5)  # 3mins * 5 times(matches)
+        #     iterate = 0
+
+        # //div[@class="week-row"]/child::span
         # await realnaps_tab.get_by_role('button', name='Previous Predictions').click()
         # # await expect(realnaps_tab.locator('//div[@id="prevHolder"]//table').nth(0)).to_be_visible(timeout=default_timeout)
         # await realnaps_tab.locator('//select[@id="season"]').select_option(label="Current Season")
@@ -76,8 +102,8 @@ async def run(playwright: Playwright):
 
 
 
-        input("End of code: ")
-        break
+        # input("End of code: ")
+        # break
     
     await context.close()
 
